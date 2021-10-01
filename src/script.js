8d7a10052004232aec1039cf4d89ab0982a09093
MiyaPeynova <maria.peynova@hotmail.com>
2021-10-01 06:27:22 +0100

    Add func to moles and dirt

--- src/script.js
+++ src/script.js
@@ -1,3 +1,50 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+const button = document.querySelector("button");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randHole(holes) {
+  const indx = Math.floor(Math.random() * holes.length);
+  const hole = holes[indx];
+  if (hole === lastHole) {
+    console.log("Nah that's the same hole");
+    randHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randTime(200, 1000);
+  const hole = randHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+button.addEventListener("click", startGame);
+
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
